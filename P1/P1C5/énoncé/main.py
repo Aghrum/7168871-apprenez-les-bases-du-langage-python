73dcd658a01af81b21800568ba965ba61e2acd95
Aghrum <vvxghs@gmail.com>
2025-08-15 18:39:23 +0200

    Update main.py
    type de données

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom = Tonny
+age = 35
+taille = 1.80
+est_étudiant = True
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"est étudiant:{est_etudiant}")
+print(f"type nom:{taper(nom)}")
+print(f"type age:{taper(age)}")
+print(f"type taille:{taper(taille)}")
+print(f"type est étudiant:{taper(est_etudiant)}")
