f13da14a5420d7cdb999453f435ccdb793fd69fe
Aghrum <vvxghs@gmail.com>
2025-08-15 14:10:01 +0200

    Update main.py
    créer mon nom et l' Age avec modification

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+nom = Tonny
+age = 25
+print(f" je m'appelle {nom} et j'ai {age} ans,")
+age = age +10
+printf" je m'appelle {nom} et j'ai {age} ans maintenant")# Écrivez votre code ici !
